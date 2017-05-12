fcb7c1b2dc36a935109c161a815d7bbf5cc278ef
Stan van Zanten <stanzanten@hotmail.com>
2017-05-12 14:15:04 +0200

    Mijn update

--- api/routes_v1.js
+++ api/routes_v1.js
@@ -5,7 +5,7 @@ var express = require('express');
 var routes = express.Router();
 
 var mijnObject = { 
-	mijntekst: 'Hello Girlyyy!',
+	mijntekst: 'Hello Menn!',
 	label: "Nog meer tekst",
 	mijnarray: [ "tekst", "nog meer tekst", 2 ],
 	mijnobject: {
